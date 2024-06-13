b55081ba784bc36a35e6ee8bca89712cb044507c
vinycxuz <vinyby@gmail.com>
2024-06-13 09:34:04 -0300

    feat(user.model): created the user model. All the datas will explain in message after this commit

---
+++ backend/models/profanity/profanityFilter.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const profanityFilterSchema = new mongoose.Schema({
+  bannedWords:
+    [String]
+  }
+)
+
+module.exports = mongoose.model('ProfanityFilter', profanityFilterSchema);
