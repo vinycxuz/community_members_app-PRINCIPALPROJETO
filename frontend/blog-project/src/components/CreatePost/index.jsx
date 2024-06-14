1fcba3a08b0ca2180a7db5ab216c96a0d846e3a6
vinycxuz <vinyby@gmail.com>
2024-06-14 09:28:24 -0300

    feat(main.jsx): intalled react query and import it in main.jsx

--- frontend/blog-project/src/components/CreatePost/index.jsx
+++ frontend/blog-project/src/components/CreatePost/index.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const CreatePost = () => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required("Title is required"),
+      description: Yup.string().required("Description is required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+});
   return (
     <div>
-      {/* Your component content goes here */}
+      <form onSubmit={formik.handleSubmit}>
+        <input 
+          type='text' 
+          name='title'
+          {...formik.getFieldProps('title')}
+          />
+          {formik.touched.title && formik.errors.title ? (
+            <div>{formik.errors.title}</div>
+          ) : null}
+          <input
+            type='text'
+            name='description'
+            {...formik.getFieldProps('description')}
+          />
+          {formik.touched.description && formik.errors.description ? (
+            <div>{formik.errors.description}</div>
+          ) : null}
+        <button type="submit">Create</button>
+      </form>
     </div>
   );
 };
