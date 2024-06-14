6886c95f60806729c7f7546cd1dddca85743c8c5
vinycxuz <vinyby@gmail.com>
2024-06-14 09:45:11 -0300

    feat(CreatePost): insert the useMutation from reactQuery to manipulate between the form and axios POST request using createPosts function

--- frontend/blog-project/src/API/posts/postsAPI.js
+++ frontend/blog-project/src/API/posts/postsAPI.js
@@ -3,6 +3,7 @@ import axios  from 'axios';
 export const createPosts = async (post) => {
     try {
         const response = await axios.post('http://localhost:5000/posts', post);
+        console.log(response);
         return response.data;
     } catch (error) {
         console.error(error);
