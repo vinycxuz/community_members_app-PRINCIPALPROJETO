b06724eae5c569c4f2f4fba2c654ccbe85cd8c4a
vinycxuz <vinyby@gmail.com>
2024-06-16 12:51:10 -0300

    feat(App, NavBar): installed a react-router-dom, apply for CreatePost and List post using a new component called NavBar.

--- frontend/blog-project/src/App.jsx
+++ frontend/blog-project/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import CreatePost from './components/CreatePost'
 import PostsList from './components/PostsList'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <CreatePost />
-     <PostsList />
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route element={<CreatePost />} path="/create-post" />
+        <Route element={<PostsList />} path="/" />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/blog-project/src/components/NavBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/create-post">Create Post</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
