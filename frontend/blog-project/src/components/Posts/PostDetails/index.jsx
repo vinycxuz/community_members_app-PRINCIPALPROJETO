0720927edbbc0511b6b3bd2663b05042c914e9c6
vinycxuz <vinyby@gmail.com>
2024-06-16 15:35:24 -0300

    feat(postsAPI): created a getPost fetch using id for search in database the specific item to manipulate

--- frontend/blog-project/src/components/Posts/PostDetails/index.jsx
+++ frontend/blog-project/src/components/Posts/PostDetails/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const PostDetails = () => {
+  const { id }  = useParams();
   return (
     <div>
       {/* Add your component content here */}
