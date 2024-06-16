8c526acdb72b9054ffdc1a526d3232c78a558f76
vinycxuz <vinyby@gmail.com>
2024-06-16 16:25:27 -0300

    feat(updatePost): increased enableReinitialize because when reload the page, we can keep the last informations in inputs

--- frontend/blog-project/src/components/Posts/UpdatePost/index.jsx
+++ frontend/blog-project/src/components/Posts/UpdatePost/index.jsx
@@ -23,6 +23,7 @@ const UpdatePost = () => {
       title: data?.title || "",
       description: "",
     },
+    enableReinitialize: true,
     validationSchema: Yup.object({
       title: Yup.string().required("Title is required"),
       description: Yup.string().required("Description is required"),
