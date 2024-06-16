c038ef41801a6c06b428c6fd630e56ae4c52d87d
vinycxuz <vinyby@gmail.com>
2024-06-16 11:51:54 -0300

    feat(PostsList): use reactQuery to manipulate the getPosts fetch using queryFn and show the datas in component, appllying status about requisitopn using isLoading, isSucess and error

--- frontend/blog-project/src/App.jsx
+++ frontend/blog-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import CreatePost from './components/CreatePost'
-import PostsList from './components/PostList'
+import PostsList from './components/PostsList'
 
 
 function App() {
--- frontend/blog-project/src/components/PostsList/index.jsx
+++ frontend/blog-project/src/components/PostsList/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from '../../API/posts/postsAPI';
 
 const PostsList = () => {
+  const { isLoading, data, error, isSuccess } = useQuery({
+    queryKey: ['list-Posts'],
+    queryFn: getPosts,
+  })
+  
   return (
     <div>
-      Posts
+      {isLoading && <p>Loading...</p>}
+      {isSuccess && <p>Posts</p>}  
+      {error && <p>Error</p>}
+    {data && data.map((post) => (
+      <div key={post._id}>
+        <h2>{post.title}</h2>
+        <p>{post.description}</p>
+      </div>
+    ))}
     </div>
   );
 };
