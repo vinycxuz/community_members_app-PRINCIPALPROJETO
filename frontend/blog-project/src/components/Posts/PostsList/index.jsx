1345228958f73ef9d0f3abe1b8530f253aa62c50
vinycxuz <vinyby@gmail.com>
2024-06-16 14:51:16 -0300

    feat(PostsList): create a link to go the specific post that you can edit

--- frontend/blog-project/src/components/Posts/PostsList/index.jsx
+++ frontend/blog-project/src/components/Posts/PostsList/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query'
 import { getPosts } from '../../../API/posts/postsAPI';
+import { Link } from 'react-router-dom';
 
 const PostsList = () => {
   const { isLoading, data, error, isSuccess } = useQuery({
@@ -17,6 +18,9 @@ const PostsList = () => {
       <div key={post._id}>
         <h2>{post.title}</h2>
         <p>{post.description}</p>
+        <Link to={`/posts/${post?._id}`}>
+          <button>Edit</button>
+        </Link>
       </div>
     ))}
     </div>
