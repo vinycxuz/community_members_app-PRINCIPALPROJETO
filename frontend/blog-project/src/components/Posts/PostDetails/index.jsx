ad21ddd6834b85a8967ef54836cc6fb3263e992c
vinycxuz <vinyby@gmail.com>
2024-06-16 15:48:54 -0300

    feat(postDetails): using react query to use getPost method for show in return details to the data, applied

--- frontend/blog-project/src/components/Posts/PostDetails/index.jsx
+++ frontend/blog-project/src/components/Posts/PostDetails/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getPost } from '../../../API/posts/postsAPI';
 
 const PostDetails = () => {
   const { id }  = useParams();
+
+  const { isLoading, data, error, isSuccess } = useQuery({
+    queryKey: ['post-details'],
+    queryFn: () => getPost(id),
+  });
+  console.log(data);
   return (
     <div>
-      {/* Add your component content here */}
+      {isLoading && <p>Loading...</p>}
+      {isSuccess && <p>Post</p>}
+      {error && <p>Error</p>}
+      {data && (
+        <div>
+          <h2>{data.title}</h2>
+          <p>{data.description}</p>
+        </div>
+      )}
     </div>
   );
 };
