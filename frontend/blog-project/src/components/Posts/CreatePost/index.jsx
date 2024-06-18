fb122f8c56e941cc714b797a3df587f192870e77
vinycxuz <vinyby@gmail.com>
2024-06-18 11:01:24 -0300

    feat(CreatePost): implemented React Quill to edit in post blog font, cover. For this, we use the component ReactQuill inside the form and using formik to manipulate and send this information as a HTML. This we helpful after to editing the tags using CSS

--- frontend/blog-project/src/components/Posts/CreatePost/index.jsx
+++ frontend/blog-project/src/components/Posts/CreatePost/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useMutation } from '@tanstack/react-query';
 import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 import { createPost } from '../../../API/posts/postsAPI';
 
 const CreatePost = () => {
+
+  const [description, setDescription] = useState("");
+
   const postMutation = useMutation({
     mutationKey: ['create-post'],
     mutationFn: createPost,
@@ -40,6 +44,13 @@ const CreatePost = () => {
       {isError && <div>Error...</div>}
       {isSuceess && <div>Post created</div>}
       <form onSubmit={formik.handleSubmit}>
+        <ReactQuill
+          value={formik.values.description}
+          onChange={(value) => {
+            setDescription(value)
+            formik.setFieldValue("description", value)
+          }} 
+          />
           {formik.touched.description && formik.errors.description ? (
             <div>{formik.errors.description}</div>
           ) : null}
