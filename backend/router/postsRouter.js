9921289276ae24a45f43ab68fe060905e92a7006
vinycxuz <vinyby@gmail.com>
2024-06-18 10:44:11 -0300

    feat(TITLE): When we will apply React Quill to create our posts, delete all about Title to will start the implement React Quill,

--- backend/router/postsRouter.js
+++ backend/router/postsRouter.js
@@ -1,52 +1,16 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 const postRouter = express.Router();
 
-const Post = require('../models/post/Post.model');
+const postController = require('../controllers/postController');
 
-postRouter.post('/api/posts/create', asyncHandler (async (req, res, next) => {
-  const postCreated = await Post.create(req.body);
-  res.status(200).json(postCreated);
-}));
+postRouter.post('/api/posts/create', postController.createPost);
 
-postRouter.get('/api/posts', asyncHandler(async (req, res) => {
-  const posts = await Post.find();
-  res.status(200).json(posts);
-}));
+postRouter.get('/api/posts', postController.getPosts);
 
-postRouter.put('/api/posts/update/:id', asyncHandler(async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
-  }
-  const postUpdated = await Post.findByIdAndUpdate(
-    req.params.id,
-    {
-      title: req.body.title,
-      description: req.body.description,
-    },
-    { 
-      new: true 
-    }
-  );
-  res.status(200).json(postUpdated);
-}));
+postRouter.put('/api/posts/update/:id', postController.updatePost);
 
-postRouter.get('/api/posts/:id', asyncHandler(async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
-  }
-  res.status(200).json(post)
-}));
+postRouter.get('/api/posts/:id', postController.getPost);
 
-postRouter.delete('/api/posts/delete/:id', asyncHandler (async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
-  }
-  await Post.findByIdAndDelete(req.params.id);
-  res.status(200).json({ message: 'Post deleted' });
-}));
+postRouter.delete('/api/posts/delete/:id', postController.deletePost);
 
 module.exports = postRouter;
--- frontend/blog-project/src/components/Posts/CreatePost/index.jsx
+++ frontend/blog-project/src/components/Posts/CreatePost/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useMutation } from '@tanstack/react-query';
+import ReactQuill from 'react-quill';
+
 import { createPost } from '../../../API/posts/postsAPI';
 
 const CreatePost = () => {
@@ -11,16 +13,13 @@ const CreatePost = () => {
   });
   const formik = useFormik({
     initialValues: {
-      title: "",
       description: "",
     },
     validationSchema: Yup.object({
-      title: Yup.string().required("Title is required"),
       description: Yup.string().required("Description is required"),
     }),
     onSubmit: (values) => {
       const post = {
-        title: values.title,
         description: values.description,
       };
 
@@ -41,19 +40,6 @@ const CreatePost = () => {
       {isError && <div>Error...</div>}
       {isSuceess && <div>Post created</div>}
       <form onSubmit={formik.handleSubmit}>
-        <input 
-          type='text' 
-          name='title'
-          {...formik.getFieldProps('title')}
-          />
-          {formik.touched.title && formik.errors.title ? (
-            <div>{formik.errors.title}</div>
-          ) : null}
-          <input
-            type='text'
-            name='description'
-            {...formik.getFieldProps('description')}
-          />
           {formik.touched.description && formik.errors.description ? (
             <div>{formik.errors.description}</div>
           ) : null}
