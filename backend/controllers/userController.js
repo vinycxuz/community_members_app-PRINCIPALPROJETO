7bbdf25640c47d79470ef22eedb659256512dc70
vinycxuz <vinyby@gmail.com>
2024-06-21 11:31:40 -0300

    feat(userController): create the module that register a new user in database. Verify first if already user exist, after this, using bcrypt for password and register the user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,33 @@
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 
 const userController = {
-  
+  register: asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    const userExists = await User.findOne({
+      email, username
+    });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userRegistered = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      _id: userRegistered._id,
+      username: userRegistered.username,
+      email: userRegistered.email,
+      token: null
+    });
+  }),
 }
+
+module.exports = userController;
